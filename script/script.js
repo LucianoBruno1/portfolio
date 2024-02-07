d6a1c536fba00b9550987146d85766159182ae60
Luciano Bruno <luciaanobruno@gmail.com>
2024-02-06 21:04:54 -0300

    finalização estilo

--- script/script.js
+++ script/script.js
@@ -1,4 +1,33 @@
-function abrirDialog(id) {
-    var modal = document.getElementById(id);
-    modal.showModal();
-}
+let btnMenu = document.getElementById('btn-menu');
+let menu = document.getElementById('menu-mobile');
+let overlay = document.getElementById('overlay-menu');
+
+btnMenu.addEventListener('click', () =>{
+    menu.classList.add('abrir-menu');
+})
+
+menu.addEventListener('click', () =>{
+    menu.classList.remove('abrir-menu');
+})
+overlay.addEventListener('click', () =>{
+    menu.classList.remove('abrir-menu');
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+    var scroll = new SmoothScroll('a[href*="#"]', {
+        speed: 800, 
+        speedAsDuration: true
+    });
+    scrollLinks.forEach(function (link) {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            
+            var targetId = this.getAttribute('href').substring(1);
+            var targetElement = document.getElementById(targetId);
+            
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
